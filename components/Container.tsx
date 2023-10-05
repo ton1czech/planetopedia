46e6bcaea93b63d80a335bb8ea392249204a5276
ton1czech <tonyasek007@gmail.com>
2023-10-05 22:11:50 +0200

    🎁NEW: container

---
+++ components/Container.tsx
@@ -0,0 +1,19 @@
+import { cn } from '@/lib/utils'
+
+interface ContainerProps {
+  children: React.ReactNode
+  className?: string
+}
+
+const Container: React.FC<ContainerProps> = ({ children, className }) => (
+  <div
+    className={cn(
+      '2xl:max-w-screen-2xl xl:max-w-screen-xl mx-auto px-3 overflow-hidden',
+      className
+    )}
+  >
+    {children}
+  </div>
+)
+
+export default Container
