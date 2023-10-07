bf8732780e958e441b37bc81ff205f7c1833ebe9
ton1czech <tonyasek007@gmail.com>
2023-10-07 22:40:38 +0200

    👌IMPROVE: hero

--- components/homepage/Hero.tsx
+++ components/homepage/Hero.tsx
@@ -3,7 +3,6 @@
 import Earth from '@/components/3d/Earth'
 import Container from '@/components/Container'
 import { Montserrat, Caramel } from 'next/font/google'
-import { TypeAnimation } from 'react-type-animation'
 import { motion } from 'framer-motion'
 import { useLanguage } from '@/store/useLanguage'
 
@@ -17,33 +16,23 @@ const Hero = () => {
       <motion.h1
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
-        transition={{ delay: 0.3, duration: 0.5 }}
+        transition={{ delay: 0.1, duration: 0.5 }}
         className={`${montserrat.className} font-bold text-3xl sm:text-4xl md:text-5xl lg:text-6xl 
-                      drop-shadow-lg text-stroke text-white hero-shadow absolute left-1/2 top-1/2 translate-x-[-50%]
+                      text-white absolute left-1/2 top-1/2 translate-x-[-50%]
                       translate-y-[-100%] z-10 pointer-events-none select-none`}
       >
         PLANETOPEDIA
       </motion.h1>
-      <p
+      <motion.p
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ delay: 0.5, duration: 1.5 }}
         className={`${caramel.className} text-center text-3xl md:text-4xl lg:text-5xl xl:text-6xl 2xl:text-7xl 
                       text-gold drop-shadow-sm  absolute left-1/2 top-1/2 translate-x-[-50%]
                       translate-y-[-25%] z-10 pointer-events-none whitespace-nowrap select-none`}
       >
-        {language === 'en' && (
-          <TypeAnimation
-            sequence={[700, 'More than an agency']}
-            speed={15}
-            cursor={false}
-          />
-        )}
-        {language === 'cs' && (
-          <TypeAnimation
-            sequence={[700, 'Více než agentura']}
-            speed={15}
-            cursor={false}
-          />
-        )}
-      </p>
+        More than an agency
+      </motion.p>
 
       <Earth />
     </Container>
