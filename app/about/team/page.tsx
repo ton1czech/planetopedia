b47fca8482ac336a3f733cedff961f2cbedbba6e
ton1czech <tonyasek007@gmail.com>
2023-10-08 14:55:44 +0200

    🎁NEW: team multilang

--- app/about/team/page.tsx
+++ app/about/team/page.tsx
@@ -14,8 +14,14 @@ export default function Team() {
         <Member
           src='/team/dominik-grossl.webp'
           name='Dominik Grössl'
-          role='Founder & CEO'
-          body='I am Dominik, founder of Travel Magazine Planetopedia with a focus on digital marketing, content creation and collaborating with luxury brands. In recent years, I have been blessed and given the opportunity to work with many photographers, creators and collaborate with many prestigious and luxurious hotels around the world such as Four Seasons, Shangri-La, Sheraton,.. I have been able to not only turn my dreams of travelling into a reality, but also successfully enter the travel industry throughout social media. Now through all the Planetopedia services I am able to inspire and build an audience in order to provide value to others.'
+          role={
+            language === 'en' ? 'Founder & CEO' : 'Zakladatel a Výkonný Ředitel'
+          }
+          body={
+            language === 'en'
+              ? 'I am Dominik, founder of Travel Magazine Planetopedia with a focus on digital marketing, content creation and collaborating with luxury brands. In recent years, I have been blessed and given the opportunity to work with many photographers, creators and collaborate with many prestigious and luxurious hotels around the world such as Four Seasons, Shangri-La, Sheraton,.. I have been able to not only turn my dreams of travelling into a reality, but also successfully enter the travel industry throughout social media. Now through all the Planetopedia services I am able to inspire and build an audience in order to provide value to others.'
+              : 'Jsem Dominik, zakladatel cestovního časopisu Planetopedia se zaměřením na digitální marketing, tvorbu obsahu a spolupráci s luxusními značkami. V posledních letech jsem měl požehnání a příležitost pracovat s mnoha fotografy, tvůrci a spolupracovat s mnoha prestižními a luxusními hotely po celém světě, jako jsou Four Seasons, Shangri-La, Sheraton,... Podařilo se mi nejen proměnit své sny o cestování ve skutečnost, ale také úspěšně vstoupit do cestovního průmyslu prostřednictvím sociálních médií. Nyní se mi prostřednictvím všech služeb Planetopedie daří inspirovat a budovat publikum, abych mohl poskytovat hodnotu ostatním.'
+          }
           links={[
             {
               icon: Instagram,
@@ -31,8 +37,16 @@ export default function Team() {
         <Member
           src='/team/fabian-dagostin.webp'
           name='Fabian Dagostin'
-          role='Strategy & Technology'
-          body="Fabian Dagostin is a technology entrepreneur and product/marketing expert. He is the CEO & Co-Founder of Raush, the first travel booking system for Social Media that helps travel influencers & brands monetize their travel content. Prior, Fabian founded & scaled Street Life, a digital tour-guide application named by media outlets as the 'Tiktok for travel'. Previously he exited Street Inc, a virality marketing agency servicing the likes of Gucci, Bacardi, Red Bull, (Careem) (and many more/as well as governmental institutions). His products & content can be enjoyed on OSN and Wavo TV networks, Google Arts & Culture and every Emirates Airlines flight. His stories have been broadcasted globally on CNN TV, newspapers like The National, Radio 103.8 and other noteworthy mediums."
+          role={
+            language === 'en'
+              ? 'Strategy & Technology'
+              : 'Strategie a Technologie'
+          }
+          body={
+            language === 'en'
+              ? "Fabian Dagostin is a technology entrepreneur and product/marketing expert. He is the CEO & Co-Founder of Raush, the first travel booking system for Social Media that helps travel influencers & brands monetize their travel content. Prior, Fabian founded & scaled Street Life, a digital tour-guide application named by media outlets as the 'Tiktok for travel'. Previously he exited Street Inc, a virality marketing agency servicing the likes of Gucci, Bacardi, Red Bull, (Careem) (and many more/as well as governmental institutions). His products & content can be enjoyed on OSN and Wavo TV networks, Google Arts & Culture and every Emirates Airlines flight. His stories have been broadcasted globally on CNN TV, newspapers like The National, Radio 103.8 and other noteworthy mediums."
+              : 'Fabian Dagostin je technologický podnikatel a odborník na produkty a marketing. Je generálním ředitelem a spoluzakladatelem společnosti Raush, prvního cestovního rezervačního systému pro sociální média, který pomáhá cestovním influencerům a značkám zpeněžit jejich cestovní obsah. Předtím Fabian založil a rozšířil Street Life, digitální průvodcovskou aplikaci, kterou média označila za "Tiktok pro cestování". Předtím opustil Street Inc, agenturu pro virální marketing, která poskytovala služby například společnostem Gucci, Bacardi, Red Bull, (Careem) (a mnoha dalším / stejně jako vládním institucím). Jeho produkty a obsah si můžete vychutnat na televizních sítích OSN a Wavo, Google Arts & Culture a v každém letu Emirates Airlines. Jeho příběhy byly celosvětově vysílány v televizi CNN, novinách jako The National, rádiu 103,8 a dalších pozoruhodných médiích.'
+          }
           links={[
             {
               icon: Instagram,
@@ -48,8 +62,12 @@ export default function Team() {
         <Member
           src='/team/adam-vaughan.webp'
           name='Adam Vaughan'
-          role='Sales & Marketing '
-          body='Hi! I’m Adam Vaughan (aka Vonski), a world traveler, history lover, and Star Wars nerd. I appreciate the chance to show you the world through my eyes. Throughout my almost 3 decades of traveling the world, I’ve experienced so many amazing things and nothing would make me happier than inspiring you. After retiring from the US Air Force in 2018, I went to work at Facebook (now Meta) where I am a senior manager and head the product security division. My focus is on travel destinations, luxury hotels, and elevated dining experiences. Let’s all experience something remarkable together!'
+          role={language === 'en' ? 'Sales & Marketing' : 'Prodej a Marketing'}
+          body={
+            language === 'en'
+              ? 'Hi! I’m Adam Vaughan (aka Vonski), a world traveler, history lover, and Star Wars nerd. I appreciate the chance to show you the world through my eyes. Throughout my almost 3 decades of traveling the world, I’ve experienced so many amazing things and nothing would make me happier than inspiring you. After retiring from the US Air Force in 2018, I went to work at Facebook (now Meta) where I am a senior manager and head the product security division. My focus is on travel destinations, luxury hotels, and elevated dining experiences. Let’s all experience something remarkable together!'
+              : 'Ahoj! Jsem Adam Vaughan (alias Vonski), světoběžník, milovník historie a Star Wars. Vážím si možnosti ukázat vám svět svýma očima. Během svých téměř tří desetiletí cestování po světě jsem zažil spoustu úžasných věcí a nic mě neudělá šťastnějším, než když vás budu inspirovat. Po odchodu z amerického letectva v roce 2018 jsem odešel pracovat do společnosti Facebook (nyní Meta), kde zastávám pozici senior manažera a vedu oddělení zabezpečení produktů. Zaměřuji se na cestovatelské destinace, luxusní hotely a nadstandardní gastronomické zážitky. Pojďme všichni společně zažít něco pozoruhodného!'
+          }
           links={[
             {
               icon: Instagram,
@@ -66,10 +84,12 @@ export default function Team() {
         <Member
           src='/team/felipe-prieto.webp'
           name='Felipe Prieto'
-          role='Community Manager'
-          body="I am Felipe Prieto, photographer and filmmaker based out in Philadelphia, PA. I am a travel enthusiast, who enjoys learn about new cultures and experiencing the world as it is. Traveling for work and for pleasure has broadened my perspective on life, and given me the opportunity to capture images that tell interesting stories. Whether it's the tall skyscrapers of New York City, the serene landscapes of Okinawa, or the vibrant colors of the colonial streets of Cartagena.
-Through the 8 years of my career I have worked with multiple brands and companies like; RedBull, JanSport, Hisea Outdoor Boots, MTV, Tamron Lenses and much more.
-I am grateful for everything that photography has brought into my life so far, and I can't wait to see where this passion takes me next. Photography will always be a big part of my life, and I hope to continue exploring the world, capturing its essence through my lens."
+          role={language === 'en' ? 'Community Manager' : 'Manažer Komunity'}
+          body={
+            language === 'en'
+              ? "I am Felipe Prieto, photographer and filmmaker based out in Philadelphia, PA. I am a travel enthusiast, who enjoys learn about new cultures and experiencing the world as it is. Traveling for work and for pleasure has broadened my perspective on life, and given me the opportunity to capture images that tell interesting stories. Whether it's the tall skyscrapers of New York City, the serene landscapes of Okinawa, or the vibrant colors of the colonial streets of Cartagena. Through the 8 years of my career I have worked with multiple brands and companies like; RedBull, JanSport, Hisea Outdoor Boots, MTV, Tamron Lenses and much more. I am grateful for everything that photography has brought into my life so far, and I can't wait to see where this passion takes me next. Photography will always be a big part of my life, and I hope to continue exploring the world, capturing its essence through my lens."
+              : 'Jsem Felipe Prieto, fotograf a filmař z Filadelfie. Jsem nadšenec do cestování, který rád poznává nové kultury a zažívá svět takový, jaký je. Cestování za prací i zábavou mi rozšířilo pohled na život a dalo mi příležitost zachytit snímky, které vyprávějí zajímavé příběhy. Ať už jsou to vysoké mrakodrapy New Yorku, klidná krajina Okinawy nebo zářivé barvy koloniálních ulic Cartageny. Za 8 let své kariéry jsem spolupracoval s mnoha značkami a společnostmi, jako jsou: RedBull, JanSport, Hisea Outdoor Boots, MTV, Tamron Lenses a mnoho dalších. Jsem vděčný za vše, co mi fotografování doposud přineslo do života, a nemohu se dočkat, kam mě tato vášeň zavede dál. Fotografie bude vždy velkou součástí mého života a doufám, že budu i nadále objevovat svět a zachycovat jeho podstatu prostřednictvím svého objektivu.'
+          }
           links={[
             {
               icon: Instagram,
@@ -85,8 +105,12 @@ I am grateful for everything that photography has brought into my life so far, a
         <Member
           src='/team/francesco-de-franco.webp'
           name='Francesco De Franco'
-          role='Community Manager'
-          body="My name is Francesco, I am a photographer and drone pilot. I collaborate with some companies for product photography and some events. I am a lover of the Nordic countries and I only feel alive when I walk in nature. If you want to go trekking in Iceland I'm up for it!"
+          role={language === 'en' ? 'Community Manager' : 'Manažer Komunity'}
+          body={
+            language === 'en'
+              ? "My name is Francesco, I am a photographer and drone pilot. I collaborate with some companies for product photography and some events. I am a lover of the Nordic countries and I only feel alive when I walk in nature. If you want to go trekking in Iceland I'm up for it!"
+              : 'Jmenuji se Francesco, jsem fotograf a pilot dronu. Spolupracuji s některými společnostmi na produktových fotografiích a některých akcích. Jsem milovníkem severských zemí a cítím se živý, jen když se procházím přírodou. Pokud chcete vyrazit na trek na Island, jsem pro!'
+          }
           links={[
             {
               icon: Instagram,
@@ -98,8 +122,12 @@ I am grateful for everything that photography has brought into my life so far, a
         <Member
           src='/team/drew-peden.webp'
           name='Drew Peden'
-          role='Community Manager'
-          body="I'm Drew Peden, a Content Creator based in South Carolina! I’ve been a creator since 2016 and continue to grow my skills for this passion every single day! Currently I am working as a full time Content Creator for a marketing agency in Greenville, South Carolina while still pursuing freelance travel photography gigs on the side! My biggest passions are landscape / adventure lifestyle photography, because I love to take people on all of my crazy adventures! I hope to continue to grow and inspire others to follow their passions and explore this beautiful planet we call home!"
+          role={language === 'en' ? 'Community Manager' : 'Manažer Komunity'}
+          body={
+            language === 'en'
+              ? "I'm Drew Peden, a Content Creator based in South Carolina! I’ve been a creator since 2016 and continue to grow my skills for this passion every single day! Currently I am working as a full time Content Creator for a marketing agency in Greenville, South Carolina while still pursuing freelance travel photography gigs on the side! My biggest passions are landscape / adventure lifestyle photography, because I love to take people on all of my crazy adventures! I hope to continue to grow and inspire others to follow their passions and explore this beautiful planet we call home!"
+              : 'Jsem Drew Peden, tvůrce obsahu z Jižní Karolíny! Tvůrcem jsem od roku 2016 a své dovednosti pro tuto vášeň rozvíjím každý den! V současné době pracuji na plný úvazek jako Content Creator pro marketingovou agenturu v Greenville v Jižní Karolíně a zároveň se stále věnuji cestovatelské fotografii na volné noze! Mou největší vášní je fotografování krajiny / dobrodružného životního stylu, protože miluji brát lidi na všechna svá bláznivá dobrodružství! Doufám, že se budu i nadále rozvíjet a inspirovat ostatní, aby následovali své vášně a prozkoumali tuto krásnou planetu, kterou nazýváme domovem!'
+          }
           links={[
             {
               icon: Instagram,
