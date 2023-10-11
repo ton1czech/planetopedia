b1ebcc5d32e2d2c1381e94515d71ff8493d144e1
ton1czech <tonyasek007@gmail.com>
2023-10-11 18:27:20 +0200

    👌IMPROVE: services section

--- components/for-creators/Hero.tsx
+++ components/for-creators/Hero.tsx
@@ -1,6 +1,11 @@
+'use client'
+
+import { useLanguage } from '@/store/useLanguage'
 import Container from '../Container'
 
 const Hero = () => {
+  const { language } = useLanguage(state => state)
+
   return (
     <div className='w-screen h-[90vh] lg:h-screen'>
       <video
@@ -13,9 +18,11 @@ const Hero = () => {
         <source src='https://i.imgur.com/OXBeHNd.mp4' />
       </video>
 
-      <div className='absolute inset-0 w-full h-full pt-24 md:pt-32'>
-        <Container>
-          <h1 className='text-5xl font-bold text-red-500'>ahoj</h1>
+      <div className='absolute inset-0 w-full h-full pt-24 md:pt-32 services-gradient'>
+        <Container className='grid items-center h-full'>
+          <h1 className='text-2xl md:text-5xl font-bold uppercase'>
+            {language === 'en' ? <>For Creators</> : <>Pro Tvůrce</>}
+          </h1>
         </Container>
       </div>
     </div>
