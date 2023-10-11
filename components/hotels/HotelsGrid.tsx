a386100b543bbadc049c1d83a6ba5d7a86c5c990
ton1czech <tonyasek007@gmail.com>
2023-10-11 18:04:09 +0200

    🎁NEW: search function for hotels blog

--- components/hotels/HotelsGrid.tsx
+++ components/hotels/HotelsGrid.tsx
@@ -1,20 +1,54 @@
 'use client'
 
+import { useState } from 'react'
 import Container from '../Container'
 import Hotel from './Hotel'
+import { Input } from '../ui/input'
+import { useLanguage } from '@/store/useLanguage'
 
 interface HotelsGridProps {
   hotels: any
 }
 
 const HotelsGrid = ({ hotels }: HotelsGridProps) => {
+  const { language } = useLanguage(state => state)
+
+  const [query, setQuery] = useState<string>('')
+
+  const getFilteredHotels = (query: string, items: {}[]) => {
+    if (!query) return hotels
+
+    return hotels.filter(
+      (hotel: any) =>
+        hotel.name.toLowerCase().includes(query.toLowerCase()) ||
+        (language === 'en'
+          ? hotel.location_en.toLowerCase().includes(query.toLowerCase())
+          : hotel.location_cz.toLowerCase().includes(query.toLowerCase()))
+    )
+  }
+
+  const filteredHotels = getFilteredHotels(query, hotels)
+
   return (
     <div className='pt-24 pb-20 bg-white md:pt-32 md:pb-32'>
       <div className='max-w-5xl mx-auto'>
-        <Container className='grid w-full gap-6 md:grid-cols-2 md:gap-12'>
-          {hotels.map((hotel: any) => (
-            <Hotel hotel={hotel} />
-          ))}
+        <Container>
+          <Input
+            type='text'
+            placeholder={
+              language === 'en'
+                ? 'Search for hotels or locations'
+                : 'Hledej hotely nebo lokace'
+            }
+            onChange={e => setQuery(e.target.value)}
+            className='bg-zinc-200 p-1 border border-zinc-300/40 w-full mb-5 md:mb-10'
+          />
+
+          <div className='grid w-full gap-6 md:grid-cols-2 md:gap-12'>
+            {filteredHotels.map((hotel: any) => (
+              <Hotel hotel={hotel} />
+            ))}
+          </div>
         </Container>
       </div>
     </div>
