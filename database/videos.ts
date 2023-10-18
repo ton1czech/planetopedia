f588271fbf00b39aac27d301487a4e0a080f085a
ton1czech <tonyasek007@gmail.com>
2023-10-18 15:44:01 +0200

    👌IMPROVE: iphone mockups feedback videos

--- database/videos.ts
+++ database/videos.ts
@@ -1,12 +1,6 @@
 export const videos = [
-  'https://i.imgur.com/tcvk9na.mp4',
-  'https://i.imgur.com/5xxfGhe.mp4',
-  'https://i.imgur.com/tcvk9na.mp4',
-  'https://i.imgur.com/5xxfGhe.mp4',
-  'https://i.imgur.com/tcvk9na.mp4',
-  'https://i.imgur.com/5xxfGhe.mp4',
-  'https://i.imgur.com/tcvk9na.mp4',
-  'https://i.imgur.com/5xxfGhe.mp4',
-  'https://i.imgur.com/tcvk9na.mp4',
-  'https://i.imgur.com/5xxfGhe.mp4',
+  'https://i.imgur.com/XMPf8em.mp4',
+  'https://i.imgur.com/OjqgRPK.mp4',
+  'https://i.imgur.com/XMPf8em.mp4',
+  'https://i.imgur.com/OjqgRPK.mp4',
 ]
