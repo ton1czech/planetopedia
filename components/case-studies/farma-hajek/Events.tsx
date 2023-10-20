592e91e9ae0f6d48043b3948e459bbb6857b2b46
ton1czech <tonyasek007@gmail.com>
2023-10-20 22:38:02 +0200

    🎁NEW: events component

--- components/case-studies/farma-hajek/Events.tsx
+++ components/case-studies/farma-hajek/Events.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { useLanguage } from '@/store/useLanguage'
+import Image from 'next/image'
 
 const Events = () => {
   const { language } = useLanguage(state => state)
@@ -11,6 +12,33 @@ const Events = () => {
         {language === 'en' && <>~ Events</>}
         {language === 'cs' && <>~ Akce</>}
       </h2>
+      <p className='mb-4'>
+        {language === 'en' && (
+          <>
+            Strongman events at Farm Hájek represent a unique blend of strength,
+            endurance, and entertainment taking place in the heart of the Czech
+            countryside. These events offer not only thrilling athletic
+            challenges but also the opportunity for participants to immerse
+            themselves in the authentic agricultural environment of Farm Hájek.
+          </>
+        )}
+        {language === 'cs' && (
+          <>
+            Strongman akce na Farmě Hájek představují jedinečnou kombinaci síly,
+            vytrvalosti a zábavy, která se odehrává v srdci českého venkova.
+            Tyto akce přinášejí nejen vzrušující sportovní výzvy, ale také
+            umožňují účastníkům prožít pravé zemědělské prostředí Farmy Hájek.
+          </>
+        )}
+      </p>
+      <div className='relative w-full h-[400px] md:h-[500px] lg:h-[600px] xl:h-[700px]'>
+        <Image
+          src='/case-studies/farma-hajek/strongman.webp'
+          alt='strongman'
+          fill
+          className='object-cover object-center'
+        />
+      </div>
     </div>
   )
 }
