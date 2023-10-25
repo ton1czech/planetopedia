be4a15478186fef231f751275e8d29fd98ceff01
ton1czech <tonyasek007@gmail.com>
2023-10-25 16:07:04 +0200

    👌IMPROVE: farmahajek web changes

--- components/case-studies/farma-hajek/Web.tsx
+++ components/case-studies/farma-hajek/Web.tsx
@@ -25,6 +25,54 @@ const Web = () => {
         {language === 'de' && <>/ Web-Entwicklung</>}
       </h2>
 
+      <ul className='mb-4 md:mb-10 grid grid-cols-2 md:grid-cols-3 gap-4'>
+        <li className='text-center bg-zinc-50 rounded-md ring-1 ring-zinc-600/20 py-2 px-4 text-zinc-700'>
+          {language === 'en' && <>Modern UI</>}
+          {language === 'cs' && <>Moderní UI</>}
+          {language === 'de' && <>Moderne UI</>}
+        </li>
+        <li className='text-center bg-zinc-50 rounded-md ring-1 ring-zinc-600/20 py-2 px-4 text-zinc-700'>
+          {language === 'en' && <>Newest technology</>}
+          {language === 'cs' && <>Nejnovější technologie</>}
+          {language === 'de' && <>Die neueste Technologie</>}
+        </li>
+        <li className='text-center bg-zinc-50 rounded-md ring-1 ring-zinc-600/20 py-2 px-4 text-zinc-700'>
+          {language === 'en' && <>Otimization</>}
+          {language === 'cs' && <>Optimalizace</>}
+          {language === 'de' && <>Otimierung</>}
+        </li>
+        <li className='text-center bg-zinc-50 rounded-md ring-1 ring-zinc-600/20 py-2 px-4 text-zinc-700'>
+          {language === 'en' && <>SEO</>}
+          {language === 'cs' && <>SEO</>}
+          {language === 'de' && <>SEO</>}
+        </li>
+        <li className='text-center bg-zinc-50 rounded-md ring-1 ring-zinc-600/20 py-2 px-4 text-zinc-700'>
+          {language === 'en' && <>Mobile Responsive</>}
+          {language === 'cs' && <>Responzivní na mobilních zařízení</>}
+          {language === 'de' && <>Mobil ansprechbar</>}
+        </li>
+        <li className='text-center bg-zinc-50 rounded-md ring-1 ring-zinc-600/20 py-2 px-4 text-zinc-700'>
+          {language === 'en' && <>Newsletter</>}
+          {language === 'cs' && <>Newsletter</>}
+          {language === 'de' && <>Newsletter</>}
+        </li>
+        <li className='text-center bg-zinc-50 rounded-md ring-1 ring-zinc-600/20 py-2 px-4 text-zinc-700'>
+          {language === 'en' && <>Email Form</>}
+          {language === 'cs' && <>Email Formulář</>}
+          {language === 'de' && <>E-Mail-Formular</>}
+        </li>
+        <li className='text-center bg-zinc-50 rounded-md ring-1 ring-zinc-600/20 py-2 px-4 text-zinc-700'>
+          {language === 'en' && <>Multilingual (CZ, EN, DE)</>}
+          {language === 'cs' && <>Vícejazyčný (CZ, EN, DE)</>}
+          {language === 'de' && <>Mehrsprachig (CZ, EN, DE)</>}
+        </li>
+        <li className='text-center bg-zinc-50 rounded-md ring-1 ring-zinc-600/20 py-2 px-4 text-zinc-700'>
+          {language === 'en' && <>Interactive</>}
+          {language === 'cs' && <>Interaktivní</>}
+          {language === 'de' && <>Interaktiv</>}
+        </li>
+      </ul>
+
       {isLoading ? (
         <Skeleton className='w-full h-[230px] sm:h-[350px] md:h-[400px] lg:h-[540px] 2xl:h-[660px]' />
       ) : (
@@ -78,49 +126,6 @@ const Web = () => {
           className='relative'
         />
       )}
-
-      <ul className='mt-4 md:mt-10'>
-        <li>
-          {language === 'en' && <>- Modern UI</>}
-          {language === 'cs' && <>- Moderní UI</>}
-          {language === 'de' && <>- Moderne UI</>}
-        </li>
-        <li>
-          {language === 'en' && <>- Newest technology</>}
-          {language === 'cs' && <>- Nejnovější technologie</>}
-          {language === 'de' && <>- Die neueste Technologie</>}
-        </li>
-        <li>
-          {language === 'en' && <>- Otimization</>}
-          {language === 'cs' && <>- Optimalizace</>}
-          {language === 'de' && <>- Otimierung</>}
-        </li>
-        <li>
-          {language === 'en' && <>- SEO</>}
-          {language === 'cs' && <>- SEO</>}
-          {language === 'de' && <>- SEO</>}
-        </li>
-        <li>
-          {language === 'en' && <>- Mobile Responsive</>}
-          {language === 'cs' && <>- Responzivní na mobilních zařízení</>}
-          {language === 'de' && <>- Mobil ansprechbar</>}
-        </li>
-        <li>
-          {language === 'en' && <>- Newsletter</>}
-          {language === 'cs' && <>- Newsletter</>}
-          {language === 'de' && <>- Newsletter</>}
-        </li>
-        <li>
-          {language === 'en' && <>- Email Form</>}
-          {language === 'cs' && <>- Email Formulář</>}
-          {language === 'de' && <>- E-Mail-Formular</>}
-        </li>
-        <li>
-          {language === 'en' && <>- Multilingual (cz, en ,de)</>}
-          {language === 'cs' && <>- Vícejazyčný (cz, en, de)</>}
-          {language === 'de' && <>- Mehrsprachig (cz, en ,de)</>}
-        </li>
-      </ul>
     </div>
   )
 }
