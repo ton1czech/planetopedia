3417e40ee801a75de0a0fc2d245dd1d2e67e881e
ton1czech <tonyasek007@gmail.com>
2023-10-29 12:14:15 +0100

    🎁NEW: filter case studies

--- app/case-studies/page.tsx
+++ app/case-studies/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import Container from '@/components/Container'
+import Filter from '@/components/case-studies/Filter'
 import { Skeleton } from '@/components/ui/skeleton'
 import { caseStudies } from '@/database/case-studies'
 import { useLanguage } from '@/store/useLanguage'
@@ -11,33 +12,58 @@ import { useEffect, useState } from 'react'
 export default function CaseStudies() {
   const { language } = useLanguage(state => state)
 
-  const [isLoading, setIsLoading] = useState(true)
+  const [isLoading, setIsLoading] = useState<boolean>(true)
+  const [service, setService] = useState<string>('')
 
   useEffect(() => {
     setIsLoading(false)
   }, [])
 
+  const getFilteredCaseStudies = (service: string) => {
+    if (!service) {
+      return caseStudies
+    }
+
+    const filteredCreators = caseStudies.filter((caseStudy: any) => {
+      const matchesServices =
+        !service ||
+        (caseStudy.servicesEn && caseStudy.servicesEn.includes(service)) ||
+        (caseStudy.servicesCz && caseStudy.servicesCz.includes(service)) ||
+        (caseStudy.servicesDe && caseStudy.servicesDe.includes(service))
+
+      return matchesServices
+    })
+
+    return filteredCreators
+  }
+
+  const filteredCaseStudies = getFilteredCaseStudies(service)
+
+  console.log('service: ', service)
+
   return (
     <div className='pt-24 pb-20 bg-white md:pb-32 md:pt-32'>
       <Container>
-        <div className='flex flex-col gap-14 md:gap-24'>
-          {caseStudies.map(casestudy => (
+        <Filter setService={setService} service={service} />
+
+        <div className='flex flex-col mt-10 md:mt-20 gap-14 md:gap-24'>
+          {filteredCaseStudies.map(casestudy => (
             <Link
               href={casestudy.href}
-              className='grid group md:grid-cols-2 gap-4'
+              className='grid gap-4 group md:grid-cols-2'
             >
               <div className='flex flex-col justify-between order-2 md:order-1'>
                 <div>
-                  <h3 className='-mb-1 md:mb-1 text-2xl font-black md:text-3xl lg:text-4xl'>
+                  <h3 className='-mb-1 text-2xl font-black md:mb-1 md:text-3xl lg:text-4xl'>
                     {casestudy.title}
                   </h3>
-                  <div className='text-sm text-zinc-600 mb-2'>
+                  <div className='mb-2 text-sm text-zinc-600'>
                     {language === 'en' && <p>{casestudy.categoryEn}</p>}
                     {language === 'cs' && <p>{casestudy.categoryCz}</p>}
                     {language === 'de' && <p>{casestudy.categoryDe}</p>}
                   </div>
                 </div>
-                <div className='text-right text-zinc-800 md:text-lg md:text-left uppercase'>
+                <div className='text-right uppercase text-zinc-800 md:text-lg md:text-left'>
                   {language === 'en' && (
                     <p>
                       {casestudy.servicesEn.map(service => (
@@ -60,13 +86,13 @@ export default function CaseStudies() {
                     </p>
                   )}
                 </div>
-                <button className='px-4 py-2 text-sm transition rounded-md w-full md:w-min h-min ring-1 ring-black hover:text-white hover:bg-black whitespace-nowrap mt-4 md:mt-0'>
+                <button className='w-full px-4 py-2 mt-4 text-sm transition rounded-md md:w-min h-min ring-1 ring-black hover:text-white hover:bg-black whitespace-nowrap md:mt-0'>
                   {language === 'en' && <>Learn More</>}
                   {language === 'cs' && <>Dozvědět se více</>}
                   {language === 'de' && <>Mehr erfahren</>}
                 </button>
               </div>
-              <div className='relative w-full overflow-hidden transition duration-500 aspect-video group-hover:shadow-lg order-1 md:order-2'>
+              <div className='relative order-1 w-full overflow-hidden transition duration-500 aspect-video group-hover:shadow-lg md:order-2'>
                 {isLoading ? (
                   <Skeleton className='w-full h-full' />
                 ) : (
---
+++ components/case-studies/Filter.tsx
@@ -0,0 +1,140 @@
+'use client'
+
+import { cn } from '@/lib/utils'
+import { useLanguage } from '@/store/useLanguage'
+
+interface ItemProps {
+  labelEn: string
+  labelCz: string
+  labelDe: string
+  setService: any
+  service: string
+}
+
+const Item = ({
+  labelEn,
+  labelCz,
+  labelDe,
+  setService,
+  service,
+}: ItemProps) => {
+  const { language } = useLanguage(state => state)
+
+  const handleClick = () => {
+    if (
+      service ===
+      (language === 'en' ? labelEn : language === 'cs' ? labelCz : labelDe)
+    ) {
+      setService('')
+    } else {
+      setService(
+        language === 'en' ? labelEn : language === 'cs' ? labelCz : labelDe
+      )
+    }
+  }
+
+  return (
+    <p
+      onClick={handleClick}
+      className={cn(
+        'px-4 py-2 text-center rounded-md cursor-pointer hover:underline bg-zinc-100 hover:bg-zinc-300 grid place-content-center',
+        service ===
+          (language === 'en'
+            ? labelEn
+            : language === 'cs'
+            ? labelCz
+            : language === 'de'
+            ? labelDe
+            : '') && 'bg-zinc-300 ring-1 ring-zinc-400'
+      )}
+    >
+      {language === 'en' && <>{labelEn}</>}
+      {language === 'cs' && <>{labelCz}</>}
+      {language === 'de' && <>{labelDe}</>}
+    </p>
+  )
+}
+
+interface FilterProps {
+  service: string
+  setService: any
+}
+
+const Filter = ({ setService, service }: FilterProps) => {
+  return (
+    <div className='grid grid-cols-2 gap-x-4 gap-y-2 md:grid-cols-3 md:gap-x-8 md:gap-y-4 lg:grid-cols-4'>
+      <Item
+        labelEn='marketing'
+        labelCz='marketing'
+        labelDe='marketing'
+        setService={setService}
+        service={service}
+      />
+      <Item
+        labelEn='events'
+        labelCz='akce'
+        labelDe='veranstaltungen'
+        setService={setService}
+        service={service}
+      />
+      <Item
+        labelEn='web development'
+        labelCz='tvorba webu'
+        labelDe='web-entwicklung'
+        setService={setService}
+        service={service}
+      />
+      <Item
+        labelEn='content creation'
+        labelCz='tvorba obsahu'
+        labelDe='erstellung von inhalten'
+        setService={setService}
+        service={service}
+      />
+      <Item
+        labelEn='social media management'
+        labelCz='správa sociálních sítí'
+        labelDe='verwaltung sozialer medien'
+        setService={setService}
+        service={service}
+      />
+      <Item
+        labelEn='brand building'
+        labelCz='budování značky'
+        labelDe='markenbildung'
+        setService={setService}
+        service={service}
+      />
+      <Item
+        labelEn='strategy'
+        labelCz='strategie'
+        labelDe='strategie'
+        setService={setService}
+        service={service}
+      />
+      <Item
+        labelEn='social media influencer campaign'
+        labelCz='influencerská kampaň na sociálních sítích'
+        labelDe='social-media-influencer-kampagne'
+        setService={setService}
+        service={service}
+      />
+      <Item
+        labelEn='social media'
+        labelCz='sociální média'
+        labelDe='soziale medien'
+        setService={setService}
+        service={service}
+      />
+      <Item
+        labelEn='management'
+        labelCz='management'
+        labelDe='verwaltung'
+        setService={setService}
+        service={service}
+      />
+    </div>
+  )
+}
+
+export default Filter
