5c593e70c1497b6e0daf8fc5067cbfc6f6e67835
ton1czech <tonyasek007@gmail.com>
2023-10-31 14:05:41 +0100

    🎁NEW: creators animations

--- components/creators/CreatorsGrid.tsx
+++ components/creators/CreatorsGrid.tsx
@@ -7,6 +7,7 @@ import { Country } from './filters/Country'
 import { useLanguage } from '@/store/useLanguage'
 import { Skills } from './filters/Skills'
 import { Followers } from './filters/Followers'
+import { motion } from 'framer-motion'
 
 interface CreatorsGridProps {
   creators: any
@@ -54,20 +55,25 @@ const CreatorsGrid = ({ creators }: CreatorsGridProps) => {
   const filteredClients = getFilteredClients(countryCode, skill, followers)
 
   return (
-    <div className='pt-24 pb-20 bg-white md:pt-32 md:pb-32'>
-      <Container className='grid w-full gap-4 lg:grid-cols-3 lg:gap-12'>
-        <Country setCountryCode={setCountryCode} />
-        <Skills setSkill={setSkill} />
-        <Followers setFollowers={setFollowers} />
-      </Container>
-      <Container>
-        <div className='my-4 lg:my-8'>
-          <div className='w-full h-px bg-zinc-600/30' />
-          <p className='text-sm text-zinc-700/90'>
-            {filteredClients.length} creators
-          </p>
-        </div>
-      </Container>
+    <motion.div className='pt-24 pb-20 bg-white md:pt-32 md:pb-32'>
+      <motion.div
+        initial={{ y: 50, opacity: 0 }}
+        whileInView={{ y: 0, opacity: 1 }}
+      >
+        <Container className='grid w-full gap-4 lg:grid-cols-3 lg:gap-12'>
+          <Country setCountryCode={setCountryCode} />
+          <Skills setSkill={setSkill} />
+          <Followers setFollowers={setFollowers} />
+        </Container>
+        <Container>
+          <div className='my-4 lg:my-8'>
+            <div className='w-full h-px bg-zinc-600/30' />
+            <p className='text-sm text-zinc-700/90'>
+              {filteredClients.length} creators
+            </p>
+          </div>
+        </Container>
+      </motion.div>
 
       {filteredClients.length === 0 && (
         <Container>
@@ -83,33 +89,39 @@ const CreatorsGrid = ({ creators }: CreatorsGridProps) => {
         </Container>
       )}
 
-      <Container className='grid w-full gap-6 md:grid-cols-2 lg:grid-cols-3 md:gap-12'>
-        {filteredClients.map(
-          ({
-            image,
-            name,
-            instagram,
-            categories,
-            locationCz,
-            locationEn,
-            locationDe,
-            followers,
-          }: any) => (
-            <Creator
-              key={instagram}
-              src={image}
-              name={name}
-              instagram={instagram}
-              categories={categories}
-              locationCz={locationCz}
-              locationEn={locationEn}
-              locationDe={locationDe}
-              followers={followers}
-            />
-          )
-        )}
-      </Container>
-    </div>
+      <motion.div
+        initial={{ y: 50, opacity: 0 }}
+        whileInView={{ y: 0, opacity: 1 }}
+        transition={{ delay: 0.3 }}
+      >
+        <Container className='grid w-full gap-6 md:grid-cols-2 lg:grid-cols-3 md:gap-12'>
+          {filteredClients.map(
+            ({
+              image,
+              name,
+              instagram,
+              categories,
+              locationCz,
+              locationEn,
+              locationDe,
+              followers,
+            }: any) => (
+              <Creator
+                key={instagram}
+                src={image}
+                name={name}
+                instagram={instagram}
+                categories={categories}
+                locationCz={locationCz}
+                locationEn={locationEn}
+                locationDe={locationDe}
+                followers={followers}
+              />
+            )
+          )}
+        </Container>
+      </motion.div>
+    </motion.div>
   )
 }
 
