252058679f87b8bed545cf7b101da9ca1ea38ac1
ton1czech <tonyasek007@gmail.com>
2023-10-31 23:21:46 +0100

    👌IMPROVE: footer layout

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -16,12 +16,7 @@ const Footer = () => {
       whileInView={{ y: 0, opacity: 1 }}
     >
       <Container className='py-20'>
-        <div className='grid md:grid-cols-2 mb-10 md:mb-20 gap-y-4'>
-          <h2 className='uppercase text-2xl font-black text-white text-center md:text-left'>
-            {language === 'en' && <>follow us on social media</>}
-            {language === 'cs' && <>sleduj nás na sociálních sítích</>}
-            {language === 'de' && <>Folgen Sie uns auf den sozialen Medien</>}
-          </h2>
+        <div className='grid place-content-center mb-10 md:mb-20 gap-y-4'>
           <div className='flex gap-x-2 md:gap-x-4 items-center md:justify-end justify-center'>
             <FooterIcon
               icon={Instagram}
