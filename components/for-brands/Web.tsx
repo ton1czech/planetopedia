3832d110fceaf40deb266606d7af81bcc3a36e5e
ton1czech <tonyasek007@gmail.com>
2023-10-31 20:15:47 +0100

    🎁NEW: changing image in web

--- components/for-brands/Web.tsx
+++ components/for-brands/Web.tsx
@@ -2,8 +2,27 @@
 
 import Image from 'next/image'
 import { motion } from 'framer-motion'
+import { useEffect, useState } from 'react'
+
+const srcs = [
+  '/services/for-brands/web/1.webp',
+  '/services/for-brands/web/2.webp',
+]
 
 const Web = () => {
+  const [src, setSrc] = useState<string>(srcs[0])
+  const [index, setIndex] = useState<number>(0)
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const nextImageIndex = (index + 1) % srcs.length
+      setIndex(nextImageIndex)
+      setSrc(srcs[nextImageIndex])
+    }, 3000)
+
+    return () => clearInterval(interval)
+  }, [index, srcs])
+
   return (
     <div className='sticky top-0 snap-start scroll-mt-20 w-screen z-[60] bg-white h-[calc(100vh-80px)] grid place-content-center'>
       <motion.div
@@ -11,12 +30,7 @@ const Web = () => {
         whileInView={{ y: 0, opacity: 1 }}
         transition={{ delay: 0.5 }}
       >
-        <Image
-          src='/case-studies/farma-hajek/new-web-en.webp'
-          alt='website'
-          width={1300}
-          height={750}
-        />
+        <Image src={src} alt='website' width={1300} height={750} />
       </motion.div>
     </div>
   )
