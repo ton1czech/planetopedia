5e22cd8dc45fb4ea8c188b10cad5606d8696b620
ton1czech <tonyasek007@gmail.com>
2023-11-02 10:36:17 +0100

    👌IMPROVE: web development text

--- components/for-brands/Web.tsx
+++ components/for-brands/Web.tsx
@@ -3,6 +3,8 @@
 import Image from 'next/image'
 import { motion } from 'framer-motion'
 import { useEffect, useState } from 'react'
+import { useLanguage } from '@/store/useLanguage'
+import Container from '../Container'
 
 const srcs = [
   '/services/for-brands/web/1.webp',
@@ -10,6 +12,8 @@ const srcs = [
 ]
 
 const Web = () => {
+  const { language } = useLanguage(state => state)
+
   const [src, setSrc] = useState<string>(srcs[0])
   const [index, setIndex] = useState<number>(0)
 
@@ -25,13 +29,44 @@ const Web = () => {
 
   return (
     <div className='sticky top-0 snap-start scroll-mt-20 w-screen z-[60] bg-white h-[calc(100vh-80px)] grid place-content-center'>
-      <motion.div
-        initial={{ y: 50, opacity: 0 }}
-        whileInView={{ y: 0, opacity: 1 }}
-        transition={{ delay: 0.5 }}
-      >
-        <Image src={src} alt='website' width={1200} height={700} />
-      </motion.div>
+      <Container>
+        <motion.div
+          initial={{ y: 50, opacity: 0 }}
+          whileInView={{ y: 0, opacity: 1 }}
+          transition={{ delay: 0.5 }}
+        >
+          <h2 className='text-2xl text-center text-black md:text-3xl mb-4'>
+            {language === 'en' && <>Web Development</>}
+            {language === 'cs' && <>Tvorba Webu</>}
+            {language === 'de' && <>Web-Entwicklung</>}
+          </h2>
+          <p className='text-black text-justify lg:text-center max-w-[90ch] mx-auto mb-6'>
+            {language === 'en' && (
+              <>
+                We use cutting edge technologies to achieve maximum speed,
+                optimal display on all types of displays and excellent search
+                engine ranking through SEO optimization.
+              </>
+            )}
+            {language === 'cs' && (
+              <>
+                Využíváme nejnovější technologie k dosažení maximální rychlosti,
+                optimálního zobrazení na všech typech displejů a vynikajícího
+                umístění ve vyhledávačích díky SEO optimalizaci.
+              </>
+            )}
+            {language === 'de' && (
+              <>
+                Wir nutzen die neuesten Technologien, um maximale
+                Geschwindigkeit, optimale Darstellung auf allen Arten von
+                Bildschirmen und hervorragende Platzierungen in Suchmaschinen
+                durch SEO-Optimierung zu erreichen.
+              </>
+            )}
+          </p>
+          <Image src={src} alt='website' width={1100} height={650} />
+        </motion.div>
+      </Container>
     </div>
   )
 }
