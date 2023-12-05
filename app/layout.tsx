50fee6c47a8dcbe05ef00c4190cc494358c2e95d
ton1czech <tonyasek007@gmail.com>
2023-12-05 09:30:38 +0100

    ❌REMOVED: google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -79,20 +79,6 @@ export default function RootLayout({
         <Navbar />
         {children}
         <Footer />
-
-        <Script
-          async
-          src='https://www.googletagmanager.com/gtag/js?id=G-F2GNWDQBT8'
-        ></Script>
-        <Script>
-          {`
-             window.dataLayer = window.dataLayer || [];
-             function gtag(){dataLayer.push(arguments);}
-             gtag('js', new Date());
-
-             gtag('config', 'G-F2GNWDQBT8');
-          `}
-        </Script>
       </body>
     </html>
   )
