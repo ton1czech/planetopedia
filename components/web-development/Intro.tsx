f8bba211fa7e7e52b69e6780f2f66b4e00155e3b
ton1czech <tonyasek007@gmail.com>
2024-02-11 21:46:38 +0100

    👌IMPROVE: intro font size

--- components/web-development/Intro.tsx
+++ components/web-development/Intro.tsx
@@ -5,13 +5,13 @@ export const Intro = () => {
   return (
     <section className='relative h-[60vh] md:h-[85vh] w-full antialiased mb-20 lg:mb-40 grid place-content-center'>
       <Container>
-        <h1 className='text-center relative z-10 text-3xl text-transparent sm:text-6xl md:text-7xl xl:text-8xl 2xl:text-9xl !leading-tight bg-clip-text bg-gradient-to-b from-neutral-600 to-neutral-950'>
+        <h1 className='text-center relative z-10 text-4xl text-transparent sm:text-6xl md:text-7xl xl:text-8xl 2xl:text-9xl !leading-tight bg-clip-text bg-gradient-to-b from-neutral-600 to-neutral-950'>
           we <b>transform</b> <br /> your <b>visitors</b> into <br /> loyal{' '}
           <b>customers</b>
         </h1>
-        <p className='relative z-10 mt-10 text-center sm:text-lg md:mt-14 lg:mt-20 md:text-2xl xl:text-3xl 2xl:text-4xl text-neutral-500'>
-          <b>Web development</b> for <b>individuals</b> and <b>companies</b>,
-          elegantly
+        <p className='relative z-10 mt-10 text-center text-lg md:mt-14 lg:mt-20 md:text-2xl xl:text-3xl 2xl:text-4xl text-neutral-500'>
+          <b>Web development</b> for <b>individuals</b>{' '}
+          <br className='sm:hidden' /> and <b>companies</b>, elegantly
         </p>
       </Container>
       <BackgroundBeams />
