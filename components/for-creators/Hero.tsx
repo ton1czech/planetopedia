f089c59550817919374bbb34fd3bcf7b995a2b16
ton1czech <tonyasek007@gmail.com>
2024-02-11 21:38:21 +0100

    👌IMPROVE: for creators hero picture position

--- components/for-creators/Hero.tsx
+++ components/for-creators/Hero.tsx
@@ -11,10 +11,10 @@ const Hero = () => {
       <img
         src='/services/for-creators/hero.webp'
         alt='hero'
-        className='absolute inset-0 object-cover object-center w-full h-full'
+        className='absolute inset-0 object-cover object-top w-full h-full'
       />
 
-      <div className='absolute inset-0 w-full h-full bg-black/10'>
+      <div className='absolute inset-0 w-full h-full'>
         <Container className='grid items-center justify-center w-full h-full'>
           <div className='text-center text-white'>
             <h1 className='text-3xl font-black uppercase md:text-5xl lg:text-7xl'>
