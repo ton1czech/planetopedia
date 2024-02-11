99d8b2b12978b4b4e3bce1ddef67ce6f9ecd8cb7
ton1czech <tonyasek007@gmail.com>
2024-02-11 16:00:09 +0100

    🎁NEW: /web-development link

--- components/for-brands/Web.tsx
+++ components/for-brands/Web.tsx
@@ -5,6 +5,8 @@ import { motion } from 'framer-motion'
 import { useEffect, useState } from 'react'
 import { useLanguage } from '@/store/useLanguage'
 import Container from '../Container'
+import { Button } from '../ui/button'
+import Link from 'next/link'
 
 const srcs = [
   '/services/for-brands/web/1.webp',
@@ -36,38 +38,57 @@ const Web = () => {
           whileInView={{ y: 0, opacity: 1 }}
           transition={{ delay: 0.5 }}
         >
-          <h2 className='text-2xl text-center text-black md:text-3xl mb-4 font-bold'>
+          <h2 className='mb-6 text-2xl font-bold text-center text-black md:mb-20 -mt-28 md:text-3xl'>
             {language === 'en' && <>we develop websites, including this one</>}
             {language === 'cs' && <>vyvíjíme webové stránky, včetně této</>}
             {language === 'de' && (
               <>wir entwickeln Websites, einschließlich dieser hier</>
             )}
           </h2>
-          <p className='text-black text-justify lg:text-center max-w-[90ch] mx-auto mb-6'>
-            {language === 'en' && (
-              <>
-                We use cutting edge technologies to achieve maximum speed,
-                optimal display on all types of displays and excellent search
-                engine ranking through SEO optimization.
-              </>
-            )}
-            {language === 'cs' && (
-              <>
-                Využíváme nejnovější technologie k dosažení maximální rychlosti,
-                optimálního zobrazení na všech typech displejů a vynikajícího
-                umístění ve vyhledávačích díky SEO optimalizaci.
-              </>
-            )}
-            {language === 'de' && (
-              <>
-                Wir nutzen die neuesten Technologien, um maximale
-                Geschwindigkeit, optimale Darstellung auf allen Arten von
-                Bildschirmen und hervorragende Platzierungen in Suchmaschinen
-                durch SEO-Optimierung zu erreichen.
-              </>
-            )}
-          </p>
-          <Image src={src} alt='website' width={1100} height={650} />
+          <div className='grid md:grid-cols-[4fr_6fr] md:gap-6'>
+            <div>
+              <p className='mx-auto mb-6 text-justify text-black'>
+                {language === 'en' && (
+                  <>
+                    Our web development combines innovative technologies with a
+                    focus on speed, clear display on all devices, and high
+                    rankings in search engines thanks to SEO. Every detail is
+                    carefully considered to not only appeal to your visitors but
+                    also transform them into loyal customers. With us, you have
+                    a guarantee of success and professional support. Your online
+                    presence is our passion.
+                  </>
+                )}
+                {language === 'cs' && (
+                  <>
+                    Naše tvorba webu spojuje inovativní technologie s ohledem na
+                    rychlost, přehledné zobrazení na všech zařízeních a vysoké
+                    umístění ve vyhledávačích díky SEO. Každý detail je pečlivě
+                    promyšlen, aby váš web nejen oslovil, ale také přetvořil
+                    návštěvníky na loajální zákazníky. S námi máte záruku
+                    úspěchu a profesionální podpory. Vaše přítomnost online je
+                    naší vášní.
+                  </>
+                )}
+                {language === 'de' && <></>}
+              </p>
+              <Link href='/web-development'>
+                <Button className='w-full max-md:hidden'>
+                  {language === 'en' && 'Learn More'}
+                  {language === 'cs' && 'Dozvědět se více'}
+                  {language === 'de' && ''}
+                </Button>
+              </Link>
+            </div>
+            <Image src={src} alt='website' width={1100} height={650} />
+            <Link href='/web-development'>
+              <Button className='w-full mt-12 md:hidden'>
+                {language === 'en' && 'Learn More'}
+                {language === 'cs' && 'Dozvědět se více'}
+                {language === 'de' && ''}
+              </Button>
+            </Link>
+          </div>
         </motion.div>
       </Container>
     </div>
