eb811e026a7365c7f9df332ac30390e0797a2c4d
ton1czech <tonyasek007@gmail.com>
2024-02-12 14:42:27 +0100

    🐛BUG FIX: layout shift intro text

--- components/web-development/Intro.tsx
+++ components/web-development/Intro.tsx
@@ -5,7 +5,7 @@ export const Intro = () => {
   return (
     <section className='relative h-[60vh] md:h-[85vh] w-full antialiased mb-20 lg:mb-40 grid place-content-center'>
       <Container>
-        <h1 className='text-center relative z-10 text-4xl text-transparent sm:text-6xl md:text-7xl xl:text-9xl !leading-tight bg-clip-text bg-gradient-to-b from-neutral-600 to-neutral-950'>
+        <h1 className='text-center relative z-10 text-4xl text-transparent sm:text-6xl md:text-7xl xl:text-[7.8rem] 2xl:text-9xl !leading-tight bg-clip-text bg-gradient-to-b from-neutral-600 to-neutral-950'>
           We <b>transform</b> <br /> your <b>visitors</b> into <br /> loyal{' '}
           <b>customers.</b>
         </h1>
