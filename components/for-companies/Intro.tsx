f8e0f09e1cbdf8d4e08d4cbc5376d974c1b7d327
ton1czech <danielanthonybaudys@gmail.com>
2024-03-21 14:28:36 +0100

    👌IMPROVE: we transform... font size

--- components/for-companies/Intro.tsx
+++ components/for-companies/Intro.tsx
@@ -9,7 +9,7 @@ export const Intro = () => {
   return (
     <div className='sticky top-0 snap-start scroll-mt-20 w-screen z-10 bg-black flex items-center h-[calc(100vh-80px)]'>
       <Container className='h-[80vh] grid place-content-center'>
-        <h1 className='text-center relative z-10 text-4xl text-transparent sm:text-6xl md:text-7xl xl:text-[7.8rem] 2xl:text-9xl !leading-tight bg-clip-text bg-gradient-to-b from-neutral-200 to-neutral-500'>
+        <h1 className='text-center relative z-10 text-4xl text-transparent sm:text-6xl md:text-7xl xl:text-[7.7rem] 2xl:text-[8.2rem] !leading-tight bg-clip-text bg-gradient-to-b from-neutral-200 to-neutral-500'>
           {language === 'en' && (
             <>
               We <b>transform</b> <br /> your <b>visitors</b> into <br /> loyal{' '}
