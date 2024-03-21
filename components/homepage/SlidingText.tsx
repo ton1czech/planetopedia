8c2e073b13ce3c5f5ba0d4127072c67f09a0a206
ton1czech <danielanthonybaudys@gmail.com>
2024-03-21 21:28:52 +0100

    👌IMPROVE: @ planetopedia text

--- components/homepage/SlidingText.tsx
+++ components/homepage/SlidingText.tsx
@@ -38,30 +38,35 @@ export const SlidingText = () => {
   return (
     <AnimatePresence>
       <div className='relative w-full h-full'>
-        <p className='absolute top-1/2 left-1/2 -translate-x-1/2 text-left w-[350px] sm:w-[550px] md:w-[620px] lg:w-[750px] xl:w-[950px] 2xl:w-[1150px] text-xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl 2xl:text-6xl z-10 inline-flex flex-nowrap'>
-          {language === 'en' && <>We create </>}
-          {language === 'cs' && <>Vytváříme </>}
-          <motion.div
-            key={currentSentence}
-            initial={{ translateY: '100%' }}
-            animate={{ translateY: '0' }}
-            exit={{ translateY: '-100%' }}
-            transition={{ duration: 0.5 }}
-            className='inline-flex gap-1 md:gap-2 lg:gap-3'
-          >
-            {words.map((word, index) => (
-              <motion.span
-                key={index}
-                initial={{ opacity: 0, translateY: '100%' }}
-                animate={{ opacity: 1, translateY: '0' }}
-                exit={{ opacity: 0, translateY: '-100%' }}
-                transition={{ duration: 0.5, delay: index * 0.1 }}
-                className='inline-flex'
-              >
-                {word}{' '}
-              </motion.span>
-            ))}
-          </motion.div>
+        <p className='absolute top-[40%] left-1/2 -translate-x-1/2 text-left w-[350px] sm:w-[550px] md:w-[620px] lg:w-[750px] xl:w-[950px] 2xl:w-[1150px] text-xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl 2xl:text-6xl z-10 inline-flex flex-col flex-nowrap'>
+          <span className='flex whitespace-nowrap text-3xl md:text-4xl lg:text-5xl xl:text-6xl font-light opacity-75 text-zinc-500'>
+            @ Planetopedia,
+          </span>
+          <span className='flex'>
+            {language === 'en' && <>We create </>}
+            {language === 'cs' && <>Vytváříme </>}
+            <motion.div
+              key={currentSentence}
+              initial={{ translateY: '100%' }}
+              animate={{ translateY: '0' }}
+              exit={{ translateY: '-100%' }}
+              transition={{ duration: 0.5 }}
+              className='inline-flex gap-1 md:gap-2 lg:gap-3 font-bold'
+            >
+              {words.map((word, index) => (
+                <motion.span
+                  key={index}
+                  initial={{ opacity: 0, translateY: '100%' }}
+                  animate={{ opacity: 1, translateY: '0' }}
+                  exit={{ opacity: 0, translateY: '-100%' }}
+                  transition={{ duration: 0.5 }}
+                  className='inline-flex'
+                >
+                  {word}{' '}
+                </motion.span>
+              ))}
+            </motion.div>
+          </span>
         </p>
       </div>
     </AnimatePresence>
