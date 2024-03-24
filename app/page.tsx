d824ee2996a133ac49e19cb21331baee64567542
ton1czech <danielanthonybaudys@gmail.com>
2024-03-24 21:03:25 +0100

    👌IMPROVE: earth is overflowing

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Hero from '@/components/homepage/Hero'
 
 export default function Home() {
   return (
-    <div className='bg-black !overflow-hidden'>
+    <div className='bg-black !max-w-[100vw] overflow-hidden'>
       <Hero />
     </div>
   )
