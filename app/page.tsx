0f783b66bc7e1f76694752445519a453c94fba45
ton1czech <danielanthonybaudys@gmail.com>
2024-03-24 21:20:06 +0100

    👌IMPROVE: keep trying

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Hero from '@/components/homepage/Hero'
 
 export default function Home() {
   return (
-    <div className='bg-black !max-w-[100vw] overflow-hidden'>
+    <div className='bg-black !max-w-[100vw] overflow-hidden relative'>
       <Hero />
     </div>
   )
